74807c5bebbb8dbba8baf1881a997765a832a33d
BokeeJ <bokeejusthard@gmail.com>
2024-07-08 18:17:29 +0200

    startedProjectNutriMilica

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+
+    <div className="bg-black flex justify-center w-full h-[1000px] text-4xl">
+      <h1 className="text-blue-400 mt-10">Cao Milice. Krecemo !!</h1>
+    </div>
+
+  )
+}
+
+export default App
