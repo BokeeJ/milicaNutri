844cb78a631a4f860534b11057d2cd48d0df9b6e
BokeeJ <bokeejusthard@gmail.com>
2024-07-22 10:58:53 +0200

    google verific

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,15 +3,15 @@ import { Link, NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion'
 import Komentari from './Komentari';
 
+
 function Home() {
     const fadeInAnimationVariants = {
         initial: {
             opacity: 0,
             y: 50
-
         },
         animate: {
-            opacity: 3,
+            opacity: 1,
             y: 0,
             transition: {
                 delay: 0.5,
@@ -20,114 +20,114 @@ function Home() {
         }
     }
     return (
-        <motion.div
-
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-                duration: 0.8,
-                delay: 0.5,
-                ease: [0, 0.71, 0.2, 1.01]
-            }} className='flex box justify-center flex-col items-center'>
-            {/* slika */}
+        <div className='flex flex-col'>
             <motion.div
-                className='relative box lg:w-[900px] md:w-[500px] w-full h-[400px]'
-
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                     duration: 0.8,
                     delay: 0.5,
                     ease: [0, 0.71, 0.2, 1.01]
-                }}
-            >
-                <img className='w-full h-full rounded-md' src="/slika2024.webp" alt="Voce" />
-                <div className='absolute inset-0 bg-gradient-to-t from-white via-transparent to-transparent rounded-md'></div>
-                <div className='absolute inset-0 bg-gradient-to-b from-white via-transparent to-transparent rounded-md'></div>
-                <div className='absolute inset-0 bg-gradient-to-l from-white via-transparent to-transparent rounded-md'></div>
-                <div className='absolute inset-0 bg-gradient-to-r from-white via-transparent to-transparent rounded-md'></div>
-            </motion.div>
-            {/* o nama */}
-            <div className='flex lg:flex-row flex-col items-center'>
-                <div className=' flex flex-col items-center p-5 lg:w-[50%] shadow-md shadow-slate-500 m-5 bg-green-100'>
-                    <h1 className='text-4xl font-bold text-gray-800 font-pisano'>Vaš Uspeh, Moja Misija</h1>
-                    <ol className='list-disc text-xl p-5 font-serif'>
-                        <li>Sa velikom posvećenošću pristupam svakom pojedincu kako bismo zajedno ostvarili što bolje rezultate.</li>
-                        <li>Vaš uspeh je i moj uspeh.</li>
-                        <li>Individualni pristup, podrška i edukacija svakog pojedinca je moj put ka vašem zdravlju.</li>
-                    </ol>
-                    <Link className='border-2 border-green-600 p-2 rounded-full mt-2 font-serif font-semibold lg:w-[30%] w-[150px] justify-center flex' to={'onama'}>Više o meni</Link>
-                </div>
+                }} className='flex box justify-center flex-col items-center'>
+                {/* slika */}
                 <motion.div
-                    variants={fadeInAnimationVariants}
-                    initial='initial'
-                    whileInView='animate'
-                    viewport={{
-                        once: true,
-                    }} className='lg:w-[50%]'>
-                    <img className='h-full w-[800px] lg:w-full ' src="/slika2.webp" alt="Dijete" />
+                    className='relative box lg:w-[900px] md:w-[500px] w-full h-[400px]'
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                        duration: 0.8,
+                        delay: 0.5,
+                        ease: [0, 0.71, 0.2, 1.01]
+                    }}
+                >
+                    <img className='w-full h-full rounded-md' src="/slika2024.webp" alt="Voce" />
+                    <div className='absolute inset-0 bg-gradient-to-t from-white via-transparent to-transparent rounded-md'></div>
+                    <div className='absolute inset-0 bg-gradient-to-b from-white via-transparent to-transparent rounded-md'></div>
+                    <div className='absolute inset-0 bg-gradient-to-l from-white via-transparent to-transparent rounded-md'></div>
+                    <div className='absolute inset-0 bg-gradient-to-r from-white via-transparent to-transparent rounded-md'></div>
                 </motion.div>
-            </div>
-            {/* citat */}
-            <div className='h-[250px] bg-emerald-50 w-full flex justify-center items-center m-5'>
-                <blockquote className='text-xl text-gray-800 font-serif text-center mt-5'>
-                    "Hrana može biti najmoćniji oblik medicine ili najsporiji oblik otrova. Birajte mudro šta stavljate na tanjir, jer ima potencijal da izleči ili naškodi vašem telu i umu."
-                    <footer className='text-gray-600 font-serif text-center mt-2'>- Ann Wigmore</footer>
-                </blockquote>
-            </div>
-            {/* Komentari */}
-            <div className='w-full relative flex justify-center flex-col items-center'>
-                <div className='absolute inset-0 bg-[url("/profil.webp")] bg-cover bg-center opacity-100'></div>
-                <div className='relative w-full bg-green-600 bg-opacity-45 flex justify-center flex-col items-center p-5'>
-                    <h1 className='text-4xl font-bold text-white font-pisano'>UTISCI O MENI</h1>
-                    <Komentari />
+                {/* o nama */}
+                <div className='flex lg:flex-row flex-col items-center'>
+                    <div className=' flex flex-col items-center p-5 lg:w-[50%] shadow-md shadow-slate-500 m-5 bg-green-100'>
+                        <h1 className='text-4xl font-bold text-gray-800 font-pisano'>Vaš Uspeh, Moja Misija</h1>
+                        <ol className='list-disc text-xl p-5 font-serif'>
+                            <li>Sa velikom posvećenošću pristupam svakom pojedincu kako bismo zajedno ostvarili što bolje rezultate.</li>
+                            <li>Vaš uspeh je i moj uspeh.</li>
+                            <li>Individualni pristup, podrška i edukacija svakog pojedinca je moj put ka vašem zdravlju.</li>
+                        </ol>
+                        <Link className='border-2 border-green-600 p-2 rounded-full mt-2 font-serif font-semibold lg:w-[30%] w-[150px] justify-center flex' to={'onama'}>Više o meni</Link>
+                    </div>
+                    <motion.div
+                        variants={fadeInAnimationVariants}
+                        initial='initial'
+                        whileInView='animate'
+                        viewport={{
+                            once: true,
+                        }} className='lg:w-[50%]'>
+                        <img className='h-full w-[800px] lg:w-full ' src="/slika2.webp" alt="Dijete" />
+                    </motion.div>
                 </div>
-            </div>
-            {/* zasto kontaktirati nutricionistu */}
-            <div className='flex flex-col lg:flex-row justify-center items-center'>
-                <div className='flex flex-col items-center justify-center p-10'>
-                    <h1 className='text-2xl font-bold text-green-600 font-baskervville m-5 text-center'>Zašto konsultovati nutricionistu?</h1>
-                    <ul className='flex flex-col gap-5 text-center'>
-
-                        <li>Zato što dobijate plan ishrane koji je samo za vas i u skladu sa vašim potrebama i zdravstvenim stanjem.</li>
-                        <li>Zato što dobijate jasne smernice koje su neophodne za uvodjenje zdravih životnih navika.</li>
-                        <li>Zato što dobijate recepte za sve obroke iz jelovnika.</li>
-                        <li>Zato što ćete imati konstantnu podršku i kontinuirano praćenje na putu ka cilju gde god da se nalazite.
-                        </li>
-                        <li>Pišite mi i krenimo na put promene.</li>
-                    </ul>
+                {/* citat */}
+                <div className='h-[250px] bg-emerald-50 w-full flex justify-center items-center '>
+                    <blockquote className='text-xl text-gray-800 font-serif text-center mt-5'>
+                        "Hrana može biti najmoćniji oblik medicine ili najsporiji oblik otrova. Birajte mudro šta stavljate na tanjir, jer ima potencijal da izleči ili naškodi vašem telu i umu."
+                        <footer className='text-gray-600 font-serif text-center mt-2'>- Ann Wigmore</footer>
+                    </blockquote>
                 </div>
-                <div className='p-5'>
-                    <img className='w-[400px] h-[450px] p-5 shadow-md shadow-green-200 rounded-xl' src="/4.webp" alt="Milica Dzelebdzic" />
-                </div>
-            </div>
-            {/* Usluge */}
-            <div className='flex flex-col lg:flex-row bg-green-50 w-full h-full p-10 justify-center gap-10 items-center'>
-                <div className='flex flex-col justify-center items-center'>
-                    <h1 className='text-7xl font-pisano m-5'>Milica Dzelebdzić</h1>
-                    <h3 className='text-3xl font-baskervville m-5'>viši dijetetičar nutricionista</h3>
-                    <hr className='border border-green-600 w-[80%] m-5' />
-                    <ol className='list-disc text-xl p-5'>
-
-                        <li>Personalizovane preporuke</li>
-                        <li>Personalizovan jelovnik</li>
-                        <li>Recepte</li>
-                        <li>Online kontrole i praćenje</li>
-                        <li>Korekcije jelovnika</li>
-                        <li>Preporuke za put</li>
-                    </ol>
-                    <Link className='border-2 border-green-600 p-2 rounded-full font-serif font-semibold w-[150px] justify-center flex' to={'usluge'}>Više...</Link>
-                </div>
-                <div className="p-10 lg:ml-[200px] flex justify-center  relative overflow-hidden w-[350px] h-[500px] rounded-md shadow-md shadow-slate-400 ">
-                    <img
-                        className="absolute top-0 left-0 w-full h-full transition-transform duration-[8000ms] transform-gpu hover:scale-150 object-cover"
-                        src="/6.webp"
-                        alt="UslugeNutri"
-                    />
+                {/* Komentari */}
+                <div className='relative flex justify-center flex-col items-center h-full w-full'>
+                    <div className='absolute inset-0 bg-[url("/profil.webp")] bg-cover bg-center opacity-75'></div>
+                    <div className='relative z-10 flex flex-col items-center bg-green-600 bg-opacity-75 p-5'>
+                        <h1 className='text-4xl font-bold text-white font-pisano'>UTISCI O MENI</h1>
+                        <div>
+                            <Komentari />
+                        </div>
+                    </div>
                 </div>
 
-            </div>
-        </motion.div>
+                {/* zasto kontaktirati nutricionistu */}
+                <div className='flex flex-col lg:flex-row justify-center items-center'>
+                    <div className='flex flex-col items-center justify-center p-10'>
+                        <h1 className='text-2xl font-bold text-green-600 font-baskervville m-5 text-center'>Zašto konsultovati nutricionistu?</h1>
+                        <ul className='flex flex-col gap-5 text-center'>
+                            <li>Zato što dobijate plan ishrane koji je samo za vas i u skladu sa vašim potrebama i zdravstvenim stanjem.</li>
+                            <li>Zato što dobijate jasne smernice koje su neophodne za uvodjenje zdravih životnih navika.</li>
+                            <li>Zato što dobijate recepte za sve obroke iz jelovnika.</li>
+                            <li>Zato što ćete imati konstantnu podršku i kontinuirano praćenje na putu ka cilju gde god da se nalazite.
+                            </li>
+                            <li>Pišite mi i krenimo na put promene.</li>
+                        </ul>
+                    </div>
+                    <div className='p-5'>
+                        <img className='w-[400px] h-[450px] p-5 shadow-md shadow-green-200 rounded-xl' src="/4.webp" alt="Milica Dzelebdzic" />
+                    </div>
+                </div>
+                {/* Usluge */}
+                <div className='flex flex-col lg:flex-row bg-green-50 w-full h-full p-10 justify-center gap-10 items-center'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <h1 className='text-7xl font-pisano m-5'>Milica Dzelebdzić</h1>
+                        <h3 className='text-3xl font-baskervville m-5'>viši dijetetičar nutricionista</h3>
+                        <hr className='border border-green-600 w-[80%] m-5' />
+                        <ol className='list-disc text-xl p-5'>
+                            <li>Personalizovane preporuke</li>
+                            <li>Personalizovan jelovnik</li>
+                            <li>Recepte</li>
+                            <li>Online kontrole i praćenje</li>
+                            <li>Korekcije jelovnika</li>
+                            <li>Preporuke za put</li>
+                        </ol>
+                        <Link className='border-2 border-green-600 p-2 rounded-full font-serif font-semibold w-[150px] justify-center flex' to={'usluge'}>Više...</Link>
+                    </div>
+                    <div className="p-10 lg:ml-[200px] flex justify-center  relative overflow-hidden w-[350px] h-[500px] rounded-md shadow-md shadow-slate-400 ">
+                        <img
+                            className="absolute top-0 left-0 w-full h-full transition-transform duration-[8000ms] transform-gpu hover:scale-150 object-cover"
+                            src="/6.webp"
+                            alt="UslugeNutri"
+                        />
+                    </div>
+                </div>
+            </motion.div >
+        </div >
     );
 }
 
--- src/Pages/Komentari.jsx
+++ src/Pages/Komentari.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -12,58 +12,33 @@ import '../styles.css';
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-export default function Komentari() {
+export default function App() {
     return (
-        <>
+        <div>
             <Swiper
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 4500,
+                    delay: 2500,
                     disableOnInteraction: false,
                 }}
-
-                navigation={false}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
                 modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
-                <SwiperSlide>
-                    <div className='text-white flex flex-col justify-center items-center h-full gap-5'>
-                        <p className='text-xl font-baskervville'>Zahvaljujući Milici, uspela sam da smršam i zadržim željenu težinu. Njeni saveti su jednostavni za praćenje i zaista funkcionišu<br />bravo!</p>
-                        <hr className='text-white w-[60%]' />
-                        <h2 className='text-3xl font-baskervville'>Dragana</h2>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='text-white flex flex-col justify-center items-center h-full gap-5'>
-                        <p className='text-xl font-baskervville'>Milica je pravi profesionalac! Njeni saveti su mi pomogli da postignem ciljeve i osećam se bolje nego ikad. Toplo je preporučujem svima!</p>
-                        <hr className='text-white w-[60%]' />
-                        <h2 className='text-3xl font-baskervville'>Nevena</h2>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='text-white flex flex-col justify-center items-center h-full gap-5'>
-                        <p className='text-xl font-baskervville'>Izuzetno sam zadovoljan saradnjom sa Milicom.Uz njenu pomoć, naučio sam kako se pravilno hraniti, i odrzavati kilazu.</p>
-                        <hr className='text-white w-[60%]' />
-                        <h2 className='text-3xl font-baskervville'>Marko</h2>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='text-white flex flex-col justify-center items-center h-full gap-5'>
-                        <p className='text-xl font-baskervville'>Milica mi je pomogla da korigujem ishranu i poboljšam svoje zdravlje. Njeni programi su prilagođeni individualnim potrebama i veoma su efikasni.</p>
-                        <hr className='text-white w-[60%]' />
-                        <h2 className='text-3xl font-baskervville'>Ivana</h2>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div className='text-white flex flex-col justify-center items-center h-full gap-5'>
-                        <p className='text-xl font-baskervville'>Milica je sjajan nutricionista! Uvek je dostupna za pitanja i pruža podršku. Njeni planovi ishrane su detaljni i lako se prate..</p>
-                        <hr className='text-white w-[60%]' />
-                        <h2 className='text-3xl font-baskervville'>Jelena</h2>
-                    </div>
-                </SwiperSlide>
-
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
             </Swiper>
-        </>
+        </div>
     );
 }
